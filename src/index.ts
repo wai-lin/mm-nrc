7a9e7787521ba5a4b2da2734b49d766c0b99d76f
wai-lin <wailin.me@icloud.com>
2021-10-28 21:15:46 +0630

    fix(type): types not suggesting in js files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
 import NRCData from './data/NRC_Data.json';
 
-export type NrcState = typeof NRCData.nrcStates[0];
-export type NrcTownship = typeof NRCData.nrcTownships[0];
-export type NrcType = typeof NRCData.nrcTypes[0];
+export type NrcState = {
+  id: string;
+  code: string;
+  number: {
+    en: string;
+    mm: string;
+  };
+  name: {
+    en: string;
+    mm: string;
+  };
+};
+export type NrcTownship = {
+  id: string;
+  code: string;
+  short: {
+    en: string;
+    mm: string;
+  };
+  name: {
+    en: string;
+    mm: string;
+  };
+  stateId: string;
+  stateCode: string;
+};
+export type NrcType = {
+  id: string;
+  name: {
+    en: string;
+    mm: string;
+  };
+};
 
 /** get all nrc raw data */
 export function getNrcData(): {
